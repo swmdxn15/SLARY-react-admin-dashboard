650045317375fc1cdaf62f6ef23477c0dfae6020
Swmdwn Khakhlary <swmdwnkhakhlary@Swmdwns-MacBook-Pro.local>
2023-07-11 21:36:13 +0530

    update sidebar and home components

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -2,7 +2,20 @@ import "./sidebar.scss";
 
 const Sidebar = () => {
   return (
-    <div className="sidebar">Sidebar</div>
+    <div className="sidebar">
+      <div className="top">
+        <span className="logo">lamadmin</span>
+      </div>
+      <div className="center">
+        <ul>
+          <li><span>Dashboard</span></li>
+          <li><span>Dashboard</span></li>
+          <li><span>Dashboard</span></li>
+          <li><span>Dashboard</span></li>
+        </ul>
+      </div>
+      <div className="bottom">color options </div>
+    </div>
   )
 }
 
