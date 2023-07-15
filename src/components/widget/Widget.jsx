7bd89e5feb341e45644ac7f33246b51a29afc691
Swmdwn Khakhlary <swmdwnkhakhlary@Swmdwns-MacBook-Pro.local>
2023-07-15 22:43:33 +0530

    update widget

--- src/components/widget/Widget.jsx
+++ src/components/widget/Widget.jsx
@@ -1,10 +1,22 @@
 import "./widget.scss";
+import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
+import PersonOutlineIcon from '@mui/icons-material/PersonOutline';
 
 const Widget = () => {
   return (
     <div className="widget">
-        <div className="left">left</div>
-        <div className="right">right</div>
+        <div className="left">
+          <span className="title">USERS</span>
+          <span className="counter">21312</span>
+          <span className="link">See all users</span>
+        </div>
+        <div className="right">
+          <div className="percentage">
+            <KeyboardArrowUpIcon/>
+            20%
+          </div>
+          <PersonOutlineIcon className="icon"/>
+        </div>
     </div>
   )
 }
