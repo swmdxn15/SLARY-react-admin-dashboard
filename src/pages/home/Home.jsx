c171bef5492c5d3a77ddfa44dd498bd7e3578cb3
Swmdwn Khakhlary <swmdwnkhakhlary@Swmdwns-MacBook-Pro.local>
2023-07-16 23:09:04 +0530

    add featured and chart

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -3,6 +3,8 @@ import './home.scss';
 import Sidebar from '../../components/sidebar/Sidebar';
 import Navbar from '../../components/navbar/Navbar';
 import Widget from '../../components/widget/Widget';
+import Featured from '../../components/featured/Featured';
+import Chart from '../../components/chart/Chart';
 
 const Home = () => {
   return (
@@ -16,9 +18,13 @@ const Home = () => {
           <Widget type="earning"/>
           <Widget type="balance"/>
         </div>
+        <div className="charts">
+          <Featured/>
+          <Chart/>
+        </div>
       </div>
     </div>
   )
 }
 
-export default Home
+export default Home;
